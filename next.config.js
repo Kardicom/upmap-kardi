dc594e3bc8fa0d10e865c134b4dd295973d5ffe8
avk-dvk@nuribeko.space <avk-dvk_nuribeko>
2025-11-05 18:31:21 +0500

    Remove demo pages and switch to turbopack

--- next.config.js
+++ next.config.js
@@ -10,19 +10,7 @@ const nextConfig = {
     ],
   },
   pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'mdx'],
-  // Исключаем demo-страницы из сборки
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-    }
-    config.module = config.module || {};
-    config.module.rules = config.module.rules || [];
-    config.module.rules.push({
-      test: /src\/pages\/demo\//,
-      loader: 'ignore-loader',
-    });
-    return config;
-  },
+  turbopack: {},
 }
 
 module.exports = nextConfig
