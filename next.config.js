2e134df467bf86fecb33c27e6c8aa23b32a03c3f
avk-dvk@nuribeko.space <avk-dvk_nuribeko>
2025-11-05 18:26:39 +0500

    Add ignore-loader config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,19 @@ const nextConfig = {
     ],
   },
   pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'mdx'],
+  // Исключаем demo-страницы из сборки
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+    }
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.module.rules.push({
+      test: /src\/pages\/demo\//,
+      loader: 'ignore-loader',
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
